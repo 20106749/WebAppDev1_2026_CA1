0c9f6c6d9a2a603967fe2251d50d7c5118fa3f94
Michael Louis Langan <20106749@mail.wit.ie>
2026-03-09 00:02:19 +0000

    routes

--- routes.js
+++ routes.js
@@ -5,10 +5,14 @@ import logger from "./utils/logger.js";
 
 const router = express.Router();
 
-// add your own routes below
-
 import start from './controllers/start.js';
-router.get('/', start.createView);
+import dashboard from './controllers/dashboard.js';
+import about from './controllers/about.js';
+import details from './controllers/details.js';
 
+router.get('/', start.createView);
+router.get('/dashboard', dashboard.createView);
+router.get('/about', about.createView);
+router.get('/details/:id', details.createView);
 
 export default router;
