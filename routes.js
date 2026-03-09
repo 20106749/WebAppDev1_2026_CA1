559d1152017d999592f06e001597b228b8ccec2e
Michael Louis Langan <20106749@mail.wit.ie>
2026-03-09 00:25:13 +0000

    get it running for now

--- routes.js
+++ routes.js
@@ -6,13 +6,13 @@ import logger from "./utils/logger.js";
 const router = express.Router();
 
 import start from './controllers/start.js';
-import dashboard from './controllers/dashboard.js';
+//import dashboard from './controllers/dashboard.js';
 import about from './controllers/about.js';
-import details from './controllers/details.js';
+//import details from './controllers/details.js';
 
 router.get('/', start.createView);
-router.get('/dashboard', dashboard.createView);
+//router.get('/dashboard', dashboard.createView);
 router.get('/about', about.createView);
-router.get('/details/:id', details.createView);
+//router.get('/details/:id', details.createView);
 
 export default router;
